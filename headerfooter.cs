583ba716a40a86d0a045c0172980f8883fe75fe1
PAVAN R <82048445+Pavan5959@users.noreply.github.com>
2021-05-17 23:40:29 +0530

    Create headerfooter.cs

---
+++ headerfooter.cs
@@ -0,0 +1,25 @@
+
+<!--Style.css-->
+body
+{
+  margin:0px;
+  padding:0px;
+}
+
+<!--Header.css-->
+p
+{
+    background-color: skyblue;
+    padding: 20px;
+    margin: 0;
+}
+
+<!--Footer.css>
+p
+{
+    background-color: gray;
+    padding: 20px;
+    margin: 0;
+    position: relative;
+    bottom:0;
+}
